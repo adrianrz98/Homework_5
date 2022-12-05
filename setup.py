4f921fd4fade60659db1676ac3be2ac27d0a06a4
adrian <adrianrz9816@gmail.com>
2022-12-05 01:28:30 +0100

    add: added main, setup and init for package installing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='snowflakes',
+    version='0.1',
+    author='DSSS_Jose_Adrian',
+    author_email='adrianrz9816@gmail.com',
+    packages=find_packages(),
+    install_requires=['turtle','numpy','random']
+
+)
