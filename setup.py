d4dbb8f279e9c3ce1aa7c66bb325041b340c306a
adrian <adrianrz9816@gmail.com>
2022-12-05 02:10:30 +0100

    change: changed structure

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     name="snowflakes",
     version='0.1',
     author='DSSS_Jose_Adrian',
-    author_email='adrianrz9816@gmail.com',
-    packages=find_packages(),
     install_requires=['turtle','numpy'],
 
 )
