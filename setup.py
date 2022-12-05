59d3062ea1b7bd868ef46ba5fc4550c2cbd018b2
adrian <adrianrz9816@gmail.com>
2022-12-05 02:24:42 +0100

    Modified data

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup 
 
 setup(
     name="snowflakes",
     version='0.1',
     author='DSSS_Jose_Adrian',
-    install_requires=['turtle','numpy'],
+    install_requires=['turtle','numpy','random',],
 
 )
