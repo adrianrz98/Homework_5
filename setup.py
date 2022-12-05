1c27f7280172030925b528af76c58430ede06d35
adrian <adrianrz9816@gmail.com>
2022-12-05 02:31:59 +0100

    modified data

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup 
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(
-    name="snowflakes",
+    name='snowflakes',
     version='0.1',
     author='DSSS_Jose_Adrian',
-    install_requires=['turtle','numpy','random',],
+    author_email='adrianrz9816@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','turtles']
 
 )
